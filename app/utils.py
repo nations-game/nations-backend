513199db62c91763f1c9a0730904df86f4fa075d
chaarlottte <bruzzthehuzz1@protonmail.com>
2024-01-19 23:20:46 -0500

    used correct quotes

--- app/utils.py
+++ app/utils.py
@@ -12,4 +12,4 @@ def hash_password(password: str) -> tuple[bytes, bytes]:
 def check_password(password: str, salt: bytes, user_hash: bytes) -> bool:
     password_bytes: bytes = password.encode()
     
-    return user_hash == hashlib.pbkdf2_hmac('sha256', password_bytes, salt, 100_000)
+    return user_hash == hashlib.pbkdf2_hmac("sha256", password_bytes, salt, 100_000)
