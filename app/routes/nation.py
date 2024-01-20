355666eac0f39c8e6c3c890a523166bfb754ef0e
chaarlottte <bruzzthehuzz1@protonmail.com>
2024-01-19 23:04:21 -0500

    Updated nation model and endpoints

--- app/routes/nation.py
+++ app/routes/nation.py
@@ -6,6 +6,12 @@ nation_endpoints: Blueprint = Blueprint(
     url_prefix="/nation"
 )
 
+# TODO: Creates a nation. Sets a name, flag, etc.
 @nation_endpoints.route("/create")
 async def create():
+    ...
+
+# TODO: Returns data of specified nation, or of nation belonging to logged in user.
+@nation_endpoints.route("/nation_data")
+async def nation_data():
     ...
